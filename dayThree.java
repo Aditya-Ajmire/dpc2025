740b011536b053ebbb7bc4c08966747915214d2b
Aditya-Ajmire <ajmireaditya733@gmail.com>
2025-08-17 23:49:29 +0530

    Create dayThree.java

---
+++ dayThree.java
@@ -0,0 +1,26 @@
+public class dayThree {
+    public static int findDuplicate(int[] arr) {
+
+        int slow = arr[0];
+        int fast = arr[0];
+
+        do {
+            slow = arr[slow];
+            fast = arr[arr[fast]];
+        } while (slow != fast);
+
+        slow = arr[0];
+        while (slow != fast) {
+            slow = arr[slow];
+            fast = arr[fast];
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 3, 4, 2}; 
+        int duplicate = findDuplicate(arr);
+        System.out.println("Duplicate number: " + duplicate); 
+    }
+}
